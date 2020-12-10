f7b206efd80787e12ac66c7117534d417e91e928
rtetelbaum <rtetelbaum@mailbox.org>
2020-12-09 21:32:52 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
-// your Bomb code here!
+import React from 'React'
+
+class Bomb extends React.Component {
+	
+	state = { secondsLeft: this.props.initialCount }
+	
+	render() {
+		if (this.state.secondsLeft > 0) {
+			return (	
+				<div>
+					<h1>{this.state.secondsLeft} seconds left before I go boom!</h1>
+				</div>
+				)
+		} else {
+			return (	
+				<div>
+					<h1>Boom!</h1>
+				</div>
+				)
+		}
+	}
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,14 @@
-// your ImageSlider code here!
+import React from 'react'
+
+class ImageSlider extends React.Component {
+	
+	state = { currentSlideIndex: 0 }
+	
+	render() {
+		return (
+			<h1>I am on slide {this.state.currentSlideIndex}</h1>
+		)
+	}
+}
+
+export default ImageSlider;
